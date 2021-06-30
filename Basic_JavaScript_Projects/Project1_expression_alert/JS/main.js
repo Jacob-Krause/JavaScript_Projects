a055f6c6559cebeb8fd888e5189271f6f497f5d6
Jacob-Krause <84555913+Jacob-Krause@users.noreply.github.com>
2021-06-29 22:29:48 -0700

    Update main.js

--- Basic_JavaScript_Projects/Project1_expression_alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_expression_alert/JS/main.js
@@ -1,1 +1,14 @@
-alert("Hello, World!")
+var A="\"hello world\"";//defines variable A
+window.alert(A)//calls variable A in a pop up window 
+document.write(A);//calls variable A and is printed on the screen
+var B="concatenated"+"string"//defines variable B
+document.write(B)//calls variable B and prints it on screen
+document.write("\"Be who you are and say what you feel, "// displays dr.seuss quote on screen
++"because those who mind don\'t matter and those who matter don\'t mind.\""
++"-Dr.Seuss")
+var b="dog", c="cat"//defines variable b and c
+document.write(c)//calls variable c and prints it on screen
+var blues="I have the blues."//defines variable blues
+var blues=blues.fontcolor("blue")//gives variable blues and blue text color
+document.write(blues)//calls variable blues and prints it on screen
+document.write(3+3)//adds 3+3 and prints the sum (6) on screen
