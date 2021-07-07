9af96870db8e2c4846f2070e455537eb6f702df8
Jacob-Krause <84555913+Jacob-Krause@users.noreply.github.com>
2021-07-06 22:50:00 -0700

    create project 5

---
+++ Basic_JavaScript_Projects/Project5_comparisons/JS/main.js
@@ -0,0 +1,21 @@
+document.write(typeof "Word");//type of operator
+
+document.write("10" + 5)//coersion operator
+
+document.write(10==10)//doubel eqaul operator
+
+X=82
+Y="82"
+document.write(X===Y)//triple equal operator
+
+document.write(10>2)//greater than operator
+
+document.write(10<2)//less than operator
+
+document.write(5>2 && 10>4)//and operator
+
+document.write(5>10 || 10>20)//or operator
+
+function not_Function() {
+    document.getElementById("Not").innerHTML= ! (20>10);
+}//not operator
